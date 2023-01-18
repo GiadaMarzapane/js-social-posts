85c6b3c41ca438ecb5ff9934f6275164d82a75df
GiadaMarzapane <giada.ortesta@gmail.com>
2023-01-18 15:30:11 +0100

    Init progetto + MILESTONE 1

---
+++ js/script.js
@@ -0,0 +1,57 @@
+const posts = [
+    {
+        "id": 1,
+        "content": "New York City - The Statue of Liberty",
+        "media": "https://images.unsplash.com/photo-1457885208630-7f09c8b8ba2b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bmV3JTIweW9yayUyMGNpdHl8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+        "author": {
+            "name": "Phil Mangione",
+            "image": "https://unsplash.it/300/300?image=15"
+        },
+        "likes": 80,
+        "created": "2023-01-04"
+    },
+    {
+        "id": 2,
+        "content": "New York City - Skyline",
+        "media": "https://images.unsplash.com/photo-1432611185496-76ccd1dc5efe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+        "author": {
+            "name": "Sofia Perlari",
+            "image": "https://unsplash.it/300/300?image=10"
+        },
+        "likes": 120,
+        "created": "2022-09-03"
+    },
+    {
+        "id": 3,
+        "content": "New York City - 5th Avenue",
+        "media": "https://images.unsplash.com/photo-1584887077991-819d6566f705?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+        "author": {
+            "name": "Chiara Passaro",
+            "image": "https://unsplash.it/300/300?image=20"
+        },
+        "likes": 78,
+        "created": "2022-05-15"
+    },
+    {
+        "id": 4,
+        "content": "New York City - Manhattan",
+        "media": "https://images.unsplash.com/photo-1635818407564-71d109055a77?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+        "author": {
+            "name": "Luca Formicola",
+            "image": null
+        },
+        "likes": 56,
+        "created": "2022-04-03"
+    },
+    {
+        "id": 5,
+        "content": "New York City - Times Square",
+        "media": "https://images.unsplash.com/photo-1570739208759-286307290931?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=610&q=80",
+        "author": {
+            "name": "Alessandro Sainato",
+            "image": "https://unsplash.it/300/300?image=29"
+        },
+        "likes": 95,
+        "created": "2022-03-05"
+    }
+];
